1d803a528bf0af850675a8de3fc41d885bb51530
rajchatterz <iamraj.space@gmail.com>
2023-11-29 13:55:14 +0530

    donw with the basic style

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import React from "react";
 import StartGameScreen from "./screens/StartGameScreen";
+import { View, StyleSheet,ImageBackground } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 const App = () => {
   return (
-    
-      <StartGameScreen/>
+    <LinearGradient colors={['#ddb53f', '#4e0329']} style={styles.rootScreen}>
+      <ImageBackground source={require('./assets/Images/background.png')} resizeMode="cover" style={styles.rootScreen} imageStyle={styles.backGroundStyle}>
+        <StartGameScreen/>
+      </ImageBackground>
+
+    </LinearGradient>
     
   )
 }
 export default App
+
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+    
+  },
+  backGroundStyle: {
+    opacity:0.25
+  }
+})
